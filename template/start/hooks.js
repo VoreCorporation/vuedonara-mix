5ee34081d0d1b6ffcd57ccaaa8ed0372fb8f47ff
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2019-02-03 13:22:04 +0700

    remove laravel mix helper in views

--- template/start/hooks.js
+++ template/start/hooks.js
@@ -7,15 +7,3 @@ hooks.before.httpServer(() => {
     console.log(rendered)
   });
 })
-
-hooks.after.providersBooted(() => {
-  const View = use('Adonis/Src/View')
-  const Helpers = use('Helpers')
-
-  View.global('mix', text => {
-    if (!text) return
-    
-    const manifest = require(path.join(Helpers.publicPath(), 'mix-manifest.json'))
-    return manifest[text]
-  })
-})
