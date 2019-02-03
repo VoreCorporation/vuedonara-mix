3f6381f18428ef355a75b6a4c7664071fdeb925b
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2019-02-03 13:33:10 +0700

    adding ascii art rainbow in production only

--- template/start/hooks.js
+++ template/start/hooks.js
@@ -1,9 +1,12 @@
 const { hooks } = require('@adonisjs/ignitor')
 const art = require('ascii-art')
 const path = require('path')
+const gradient = require('gradient-string')
 
 hooks.before.httpServer(() => {
-  art.font('Vuedonara Mix', 'Doom', function (rendered) {
-    console.log(rendered)
-  });
+  if (process.env.NODE_ENV == 'production') {
+    art.font('Vuedonara Mix', 'Doom', function (rendered) {
+      console.log(gradient.rainbow(rendered))
+    })
+  }
 })
