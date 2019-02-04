ba3f6484d356dff7ca1484e5e37a5baba7f9785c
DeVoresyah ArEst <de.voresyah@gmail.com>
2019-02-05 01:02:55 +0700

    Fixed vue-router-sync

--- resources/src/app.js
+++ resources/src/app.js
@@ -19,10 +19,6 @@ const app = new Vue({
   ...App
 })
 
-// prime the store with server-initialized state.
-// the state is determined during SSR and inlined in the page markup.
-store.replaceState(window.__INITIAL_STATE__)
-
 // Change Document Title
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
